e48a838ce64c4d262a794d4b531efcd4c38db5c7
djhvscf <networsociales@hotmail.com>
2014-01-17 00:29:14 -0600

    home.html

--- Examen_1/Js/searchCtrl.js
+++ Examen_1/Js/searchCtrl.js
@@ -5,15 +5,28 @@ angular.module('search', ['mymovie'])
         this.id = 0;
         this.title = '';
         this.limit = 20;
+        var html = '';
         this.movies;
-
         this.fetch = function() {
             myMovieSvc.fetchByTitle(this.title, this.limit, function(data){
                 this.movies = data.movies;
-                window.alert(this.movies);
+                document.getElementById("iSearch").value = this.movies[0].id;
+                angular.forEach(data.movies, function(movie){
+                    html += '<tr>';
+                    html += '<td>' + movie.id +'</td>';
+                    html += '<td>' + movie.title +'</td>';
+                    html += '<td>' + movie.year +'</td>';
+                    html += '</tr>';
+                });
             });
+            debugger;
+            if(html != ''){
+                document.getElementById("tableTest").innerHTML = html;
+            }
         }
+
+        this.rowClass = function(movie){
+            debugger;
+            return movie.id;
+        };
     }]);
-
-
-
