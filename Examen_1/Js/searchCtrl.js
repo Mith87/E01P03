501d7fb79fe51f1ae05a20f1d40f85fe29c0aec2
djhvscf <networsociales@hotmail.com>
2014-01-19 02:46:34 -0600

    Nuevo diseño del search. COMPLETAMENTE FUNCIONAL

--- Examen_1/Js/searchCtrl.js
+++ Examen_1/Js/searchCtrl.js
@@ -1,44 +1,14 @@
 
 angular.module('search', ['mymovie'])
-    .controller('SearchController', [ 'myMovieSvc', function(myMovieSvc) {
-
-        this.id = 0;
-        this.title = '';
-        this.limit = 20;
-        var html = '';
-        this.movies;
-        this.fetch = function() {
+    .controller('SearchController', [ '$scope', 'myMovieSvc', function($scope, myMovieSvc) {
+        debugger;
+        $scope.id = 0;
+        $scope.title = '';
+        $scope.limit = 20;
+        $scope.movies;
+        $scope.fetch = function() {
             myMovieSvc.fetchByTitle(this.title, this.limit, function(data){
-                html = '';
-                this.movies = data.movies;
-                angular.forEach(data.movies, function(movie){
-					html += '<div class="row-fluid">';
-						html += '<div class="span2 imgMovie">';
-							html += '<img height="150" width="100" src="' + movie.posters.profile + '" class="img-circle" />';
-						html += '</div>';
-						html += '<div class="span8">';
-							html += '<h3>Movie Title: ' + movie.title + '</h3>';
-							html += '<h3>Year: ' + movie.year + '</h3>';
-						html += '</div>';
-						html += '<div class="span2 buttonAddFavorite">';
-							html +=	'<div class="btn-group">';
-								html +=	'<a class="btn dropdown-toggle btn-primary" data-toggle="dropdown" href="#">Action <span class="icon-cog icon-white"></span><span class="caret"></span></a>';
-								html += '<ul class="dropdown-menu">'
-										html += '<li><a href="#"><span class="glyphicon glyphicon-star"></span> Add to favorites</a></li>';
-										html += '<li><a target="_blank" href='+ movie.links.alternate + '><span class="glyphicon glyphicon-plus"></span> More information</a></li>';
-								html += '</ul>';
-							html += '</div>';
-						html += '</div>';
-					html += '</div>';
-                });
-                if(html != ''){
-                    document.getElementById("divContainerMovies").innerHTML = html;
-                }
+                $scope.movies = data.movies;
             });
         }
-
-        this.rowClass = function(movie){
-            debugger;
-            return movie.id;
-        };
     }]);
