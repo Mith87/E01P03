9dba71e4f3190b4e8a01f92dd076863cff08f41c
Mith87 <jnaranjo_87@hotmail.com>
2014-01-20 02:21:26 -0600

    Cambios menores en Home, logica agregar favoritos es funcional

--- Examen_1/Js/searchCtrl.js
+++ Examen_1/Js/searchCtrl.js
@@ -1,5 +1,5 @@
 angular.module('searchModule', ['myMovieModule', 'LocalStorageModule'])
-    .controller('SearchController', [ '$scope', 'myMovieSvc', 'localStorageService', function($scope, myMovieSvc, localStorageService) {
+    .controller('SearchController', [ '$scope', 'myMovieSvc', 'localStorageService', function($scope, myMovieSvc, localStorageSvc) {
         $scope.id = 0;
         $scope.title = '';
         $scope.limit = 0;
@@ -19,18 +19,32 @@ angular.module('searchModule', ['myMovieModule', 'LocalStorageModule'])
         }
 
         $scope.addFavorite = function(movie) {
-            window.alert(movie.title);
+
+            var favorites = localStorageSvc.get('favorites');
+
+            if(!favorites){
+                var JSON_STRUCT = {movies:[]};
+                JSON_STRUCT.movies.push(movie);
+                localStorageSvc.set('favorites', JSON_STRUCT);
+                window.alert(movie.title + ' added to Favorites');
+            } else {
+                var isFavorite = false;
+                angular.forEach(favorites.movies, function(m){
+                    console.log(movie.id + ' ' + m.id);
+                    if(m.id == movie.id){
+                        isFavorite = true;
+                    }
+                });
+
+                if(isFavorite){
+                    window.alert(movie.title + ' already in Favorites');
+                } else {
+                    favorites.movies.push(movie);
+                    localStorageSvc.set('favorites', favorites);
+                    window.alert(movie.title + ' added to Favorites');
+                }
+            }
+            console.log(JSON.stringify(localStorageSvc.get('favorites')));
         }
 
     }]);
-
-/*
-angular.module('favoritesModule', ['LocalStorageModule'])
-    .controller('FavoritesController', [ '$scope', 'LocalStorageService', function($scope, LocalStorageService) {
-
-        $scope.movies;
-
-
-
-    }]);
-*/
